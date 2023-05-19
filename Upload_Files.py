55f6ccd78ffcba335eb1ba6326a577c2de32c763
hajjane imad Eddine <hajjane.imade@gmail.com>
2023-05-19 16:11:08 +0200

    update Upload_Files.py

--- Upload_Files.py
+++ Upload_Files.py
@@ -8,8 +8,7 @@ PATHS = {os.path.join("Tache3_Analyse_opinion", "Transformer", "Model", "Model")
          os.path.join("Tache3_Analyse_opinion", "Bi-LSTM", "Model"):["bidirectional_lstm_NN.h5"],
          os.path.join("Tache3_Analyse_opinion", "CNN", "Model"):["CNN.h5"],
          os.path.join("Tache4_barre_de_recherche","model","LSTM","Model"):["bidirectional_lstm_NN_ranking.h5"],
-         os.path.join("Tache3_Analyse_opinion", "train"):["training_noemoticon.csv"],
-         os.path.join("result"):["sim.npy"]}
+         os.path.join("Tache3_Analyse_opinion", "train"):["training_noemoticon.csv"]}
 
 FOLDER_NAME = "Model"
 URL = "https://drive.google.com/drive/folders/1szZfxCGsK3XykrVHzD5D7B6R4a3bNf07?usp=share_link"
@@ -22,14 +21,14 @@ def download_file_from_google_drive(url):
 
 
 if __name__ == "__main__":
-  for path in PATHS:
-    if not os.path.exsist(path):
-      os.makedirs(path, exist_ok=True)
+    for path in PATHS:
+        if not os.path.exists(path):
+          os.makedirs(path, exist_ok=True)
 
-  download_file_from_google_drive(URL)
+    #download_file_from_google_drive(URL)
     for to, names in PATHS.items():
        for name in names:
           shutil.move(os.path.join(FOLDER_NAME, name), os.path.join(to, name))
-  shutil.rmtree(FOLDER_NAME)
+    shutil.rmtree(FOLDER_NAME)
 
 
